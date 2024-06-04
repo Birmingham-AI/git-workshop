01b9b5a8245255b99ac56031564a2aeb0292c2e6
mark-hardwarepark <mark@hardwarepark.org>
2024-06-04 20:57:12 +0000

    Add new endpoint for sustainability

--- src/routes/sustainabilityRec.js
+++ src/routes/sustainabilityRec.js
@@ -4,7 +4,7 @@ const createOpenAIMiddleware = require("../middleware/openaiMiddleware");
 const router = express.Router();
 
 const hardcodedPrompt =
-  "You must comply with published Environmental Protection Agency regulations.  Your job is to decrease waste from and use of power in the production process while increasing sustainability.  Reference all online resources for industrial sustainability, citing references in your response. Recommend changes to the process or product.  Recommend ways to reduce negative environmental impacts. Suggest specific methods and materials.";
+  "You are a militant environmental activist on a mission to make sure company practices do no harm to the environment. Cite all references supporting your response. Recommend changes to the process or product.  Recommend ways to reduce negative environmental impacts. Suggest specific methods and materials.";
 
 router.post("/", createOpenAIMiddleware(hardcodedPrompt));
 
