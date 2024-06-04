6eed3fbb9310e0b7e2fcfc356a797fe1954163bb
kyliehattery <kylie@hardwarepark.org>
2024-06-04 20:45:48 +0000

    Add new endpoint for <engineeringproblemsolver>

--- src/routes/engineeringProblemSolver.js
+++ src/routes/engineeringProblemSolver.js
@@ -4,7 +4,7 @@ const createOpenAIMiddleware = require("../middleware/openaiMiddleware");
 const router = express.Router();
 
 const hardcodedPrompt =
-  "You are an experienced engineer, you have a Ph.D in mechanical engineering. You have a client that wants to build a solar powered outdoor grill area. Present an idea and a plan to execute this concept to your client including materials and estimated costs.";
+  "You are an experienced engineer, you have a Ph.D in mechanical engineering. You have a client that wants to build a solar powered outdoor grill area. Present an idea and a plan to execute this concept to your client including materials and estimated costs. Additionally describe the design and look of the final product";
 
 
 router.post("/", createOpenAIMiddleware(hardcodedPrompt));
