c23a0a6febb737b8d9fee49a36d90d1a312198cb
t4hli <tahli@hardwarepark.org>
2024-06-04 20:48:43 +0000

    Add new endpoint for <yourFeature>

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 // Import (Require) your routes here
 const healthcheck = require("./routes/healthcheck");
 const testingForInterns = require("./routes/testingForInterns");
-
+const materialAdvice = require("./routes/materialAdvice");
 const app = express();
 
 app.use(express.json());
@@ -10,5 +10,6 @@ app.use(express.json());
 // Add your routes below
 app.use("/healthcheck", healthcheck);
 app.use("/testing-for-interns", testingForInterns);
+app.use("/material-advice", materialAdvice);
 
 module.exports = app;
