75d2469fa18645f150f8438d566118d22e6a2073
bhumipatel00 <bhumi@hardwarepark.org>
2024-06-04 20:46:55 +0000

    Add new endpoint for productdescriptiongenerator

--- src/routes/productdescriptiongenerator.js
+++ src/routes/productdescriptiongenerator.js
@@ -4,7 +4,7 @@ const createOpenAIMiddleware = require("../middleware/openaiMiddleware");
 const router = express.Router();
 
 const hardcodedPrompt =
-  "Generate a short description about the product's material, dimensions, features, and target market";
+  "You are a product developer with 10 years of industry experience. Generate a short description about the product's material, dimensions, features, and target market";
 
 router.post("/", createOpenAIMiddleware(hardcodedPrompt));
 
